7db3e6d30073d384bd65464eb7d4829da66d1ffe
Rais Rahman <rais@skyshi.com>
2018-01-16 11:13:08 +0700

    updated merapi

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ npm i autocannon -g
 npm install express
 npm install n
 npm install moleculer-cli -g
+npm install tsc -g
 git clone https://github.com/nestjs/javascript-starter.git nestjs
 cd nestjs
 npm install
