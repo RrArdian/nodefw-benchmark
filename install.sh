ac2ed5ab3e773c4ccf77eeb6595a0ac2486e65df
Rais Rahman <rais@skyshi.com>
2018-01-16 10:17:19 +0700

    update install command

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 npm install express
 npm install n
 npm install moleculer-cli -g
+git clone https://github.com/nestjs/javascript-starter.git nestjs
+cd project
+npm install
+cd ..
 cd merapi
 npm install
 cd ..
 cd moleculer
-npm install
-cd ..
-cd nestjs
 npm install
 cd ..
