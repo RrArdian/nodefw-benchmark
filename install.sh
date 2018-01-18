7b3c99bc9fd425a25b6aa00e926e34c3646d7131
Rais Rahman <rais@skyshi.com>
2018-01-18 09:20:01 +0700

    updated installer

--- install.sh
+++ install.sh
@@ -3,13 +3,19 @@ npm install express
 npm install n
 npm install moleculer-cli -g
 npm install tsc -g
-git clone https://github.com/nestjs/javascript-starter.git nestjs
-cd nestjs
-npm install
-cd ..
 cd merapi
 npm install
 cd ..
 cd molecular
 npm install
 cd ..
+cd nest
+npm install
+cd ..
+cd loopback2
+npm install
+cd ..
+cd loopback3
+npm install
+cd ..
+echo "Installation done!"
